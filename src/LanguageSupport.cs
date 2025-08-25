25b0d7eae3896fec633345d87f23922f5c025947
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2025-08-25 18:25:16 +0200

    correct log messages

--- src/LanguageSupport.cs
+++ src/LanguageSupport.cs
@@ -24,7 +24,7 @@ internal class LanguageSupport : Mod
 
     public LanguageSupport() : base("Language Support")
     {
-        Log("!OnChangeFontByLanguageSetFont");
+        Log("!LanguageSupport");
         Instance = this;
 
         _dir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? throw new DirectoryNotFoundException("I have no idea how you did this, but good luck figuring it out."), _folder);
@@ -42,7 +42,7 @@ internal class LanguageSupport : Mod
 
     public override string GetVersion()
     {
-        Log("!OnChangeFontByLanguageSetFont");
+        Log("!GetVersion");
         var asm = Assembly.GetExecutingAssembly();
         var ver = asm.GetName().Version.ToString();
         var sha1 = SHA1.Create();
@@ -56,7 +56,7 @@ internal class LanguageSupport : Mod
 
     public override void Initialize()
     {
-        Log("!OnChangeFontByLanguageSetFont");
+        Log("!Initialize");
         Instance = this;
 
         foreach (var t in Object.FindObjectsOfType<UnityEngine.UI.MenuLanguageSetting>()) t.RefreshControls();
@@ -136,83 +136,10 @@ internal class LanguageSupport : Mod
         var tmp = ReflectionHelper.GetField<ChangeFontByLanguage, TextMeshPro>(self, "tmpro");
         var tmpMat = _fa.material;
         tmpMat.shader = tmp.font.material.shader;
-        //tmpMat.SetTexture("_BumpMap",    null);
-        //tmpMat.SetTexture("_Cube",       null);
-        //tmpMat.SetTexture("_FaceTex",    null);
-        //tmpMat.SetTexture("_MainTex",    _fa.atlas);
-        //tmpMat.SetTexture("_OutlineTex", null);
-        //tmpMat.SetFloat("_Ambient", 0.5f);
-        //tmpMat.SetFloat("_Bevel", 0.5f);
-        //tmpMat.SetFloat("_BevelClamp", 0);
-        //tmpMat.SetFloat("_BevelOffset", 0);
-        //tmpMat.SetFloat("_BevelRoundness", 0);
-        //tmpMat.SetFloat("_BevelWidth", 0);
-        //tmpMat.SetFloat("_BumpFace", 0);
-        //tmpMat.SetFloat("_BumpOutline", 0);
-        //tmpMat.SetFloat("_ColorMask", 15);
-        //tmpMat.SetFloat("_Diffuse", 0.5f);
-        //tmpMat.SetFloat("_FaceDilate", 0);
-        //tmpMat.SetFloat("_FaceUVSpeedX", 0);
-        //tmpMat.SetFloat("_FaceUVSpeedY", 0);
-        //tmpMat.SetFloat("_GlowInner", 0.05f);
-        //tmpMat.SetFloat("_GlowOffset", 0);
-        //tmpMat.SetFloat("_GlowOuter", 0.05f);
-        //tmpMat.SetFloat("_GlowPower", 0.75f);
-        //tmpMat.SetFloat("_GradientScale", 5);
-        //tmpMat.SetFloat("_LightAngle", 3.1416f);
-        //tmpMat.SetFloat("_MaskID", 0);
-        //tmpMat.SetFloat("_MaskSoftnessX", 0);
-        //tmpMat.SetFloat("_MaskSoftnessY", 0);
-        //tmpMat.SetFloat("_OutlineSoftness", 0);
-        //tmpMat.SetFloat("_OutlineUVSpeedX", 0);
-        //tmpMat.SetFloat("_OutlineUVSpeedY", 0);
-        //tmpMat.SetFloat("_OutlineWidth", 0);
-        //tmpMat.SetFloat("_PerspectiveFilter", 0.875f);
-        //tmpMat.SetFloat("_Reflectivity", 10);
-        //tmpMat.SetFloat("_ScaleRatioA", 0.8f);
-        //tmpMat.SetFloat("_ScaleRatioB", 0.8f);
-        //tmpMat.SetFloat("_ScaleRatioC", 0.6504065f);
-        //tmpMat.SetFloat("_ScaleX", 1);
-        //tmpMat.SetFloat("_ScaleY", 1);
-        //tmpMat.SetFloat("_ShaderFlags", 0);
-        //tmpMat.SetFloat("_SpecularPower", 2);
-        //tmpMat.SetFloat("_Stencil", 0);
-        //tmpMat.SetFloat("_StencilComp", 8);
-        //tmpMat.SetFloat("_StencilOp", 0);
-        //tmpMat.SetFloat("_StencilReadMask", 255);
-        //tmpMat.SetFloat("_StencilWriteMask", 255);
-        //tmpMat.SetFloat("_TextureHeight", _fa.atlas.height);
-        //tmpMat.SetFloat("_TextureWidth", _fa.atlas.width);
-        //tmpMat.SetFloat("_UnderlayDilate", 0.23f);
-        //tmpMat.SetFloat("_UnderlayOffsetX", 0);
-        //tmpMat.SetFloat("_UnderlayOffsetY", 0);
-        //tmpMat.SetFloat("_UnderlaySoftness", 1);
-        //tmpMat.SetFloat("_VertexOffsetX", 0);
-        //tmpMat.SetFloat("_VertexOffsetY", 0);
-        //tmpMat.SetFloat("_WeightBold", 0.75f);
-        //tmpMat.SetFloat("_WeightNormal", 0);
-        //tmpMat.SetColor("_ClipRect", new Color(-10000, -10000, 10000, 10000));
-        //tmpMat.SetColor("_EnvMatrixRotation", new Color(0, 0, 0, 0));
-        //tmpMat.SetColor("_FaceColor", new Color(1, 1, 1, 1));
-        //tmpMat.SetColor("_GlowColor", new Color(0, 1, 0, 0.5f));
-        //tmpMat.SetColor("_MaskCoord", new Color(0, 0, 0, 0));
-        //tmpMat.SetColor("_OutlineCoord", new Color(0, 0, 0, 1));
-        //tmpMat.SetColor("_ReflectFaceColor", new Color(0, 0, 0, 1));
-        //tmpMat.SetColor("_ReflectOutlineColor", new Color(0, 0, 0, 1));
-        //tmpMat.SetColor("_SpecularColor", new Color(1, 1, 1, 1));
-        //tmpMat.SetColor("_UnderlayColor", new Color(0, 0, 0, 0.221f));
+
         _fa.material = tmpMat;
         tmp.font = _fa;
-        //tmp.font.atlas = _fa.atlas;
-        //Dictionary<int, TMP_Glyph> tmpGlyphs = _fa.characterDictionary;
-        //foreach (var pair in tmpGlyphs)
-        //{
-        //    pair.Value.y = (_fa.atlas.height - (pair.Value.y + (pair.Value.height / 2))) - (pair.Value.height / 2);
-        //}
-        //tmp.font.SetAttr("m_characterDictionary", tmpGlyphs);
-        //tmp.font.SetAttr("m_fontInfo", _fa.fontInfo);
-        //tmp.font.SetAttr("m_kerningDictionary", _fa.kerningDictionary);
-        //tmp.font.SetAttr("m_kerningInfo", _fa.kerningInfo);
+
         ReflectionHelper.SetField<ChangeFontByLanguage, TextMeshPro>(self, "tmpro", tmp);
         Log("~OnChangeFontByLanguageSetFont");
     }
